23ea013b11835b543d0858bc56008ff2c69b714c
MD MEHEDI HASHAN MOLLA <mdmehedihashanmolla@gmail.com>
2025-02-28 02:05:30 +0530

    init

--- test/MDToken.t.sol
+++ test/MDToken.t.sol
@@ -8,11 +8,7 @@ contract MDTokenTest is Test {
     MDToken token;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     address owner = address(1);
     address user = address(2);
